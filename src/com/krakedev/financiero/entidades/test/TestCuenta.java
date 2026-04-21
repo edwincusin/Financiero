bb8727c9c0c9c83982b31beaad2eb3a63e84990a
EDWINCSN <c.edwin32@hotmail.com>
2026-04-20 23:13:49 -0500

    EGCA: Creacion clase de prueba TestCuenta

---
+++ src/com/krakedev/financiero/entidades/test/TestCuenta.java
@@ -0,0 +1,14 @@
+package com.krakedev.financiero.entidades.test;
+
+import com.krakedev.financiero.entidades.Cuenta;
+
+public class TestCuenta {
+
+	public static void main(String[] args) {
+		//INSTANCIAR OBJETO DE LA CLASE CUENTA
+		Cuenta cuenta1=new Cuenta("001");
+		cuenta1.imprimir();
+
+	}
+
+}
